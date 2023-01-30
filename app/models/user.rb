be175ee9759e2d4f55aa6bebf9c6b778e413435a
Nauman khan <naumankhan642@gmail.com>
2023-01-30 21:22:09 +0500

    Added jwt actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :jwt_authenticatable,
+         jwt_revocation_strategy: JwtDenylist
+
   enum :role => [:user, :moderator, :admin]
   after_initialize :set_user_role, if: :new_record?
 
